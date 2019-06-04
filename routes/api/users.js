c269008606a04e0f55db15dd8c6bccea7142b040
spikeburton <ralphburton3@gmail.com>
2019-06-04 14:37:47 -0400

    implement validations for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 /**
- * @route   GET api/users
- * @desc    Test route
+ * @route   POST api/users
+ * @desc    Register user
  * @access  Public
  */
 
-router.get('/', (req, res) => {
-  res.send("User route");
-})
+router.post(
+  '/',
+  [
+    check('name', 'Name is required.')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email.').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists.' }] });
+      }
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return JSON web token
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
