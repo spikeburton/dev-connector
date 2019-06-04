8d609634371dcd0e1d87b98f056e2052d0a587d2
spikeburton <ralphburton3@gmail.com>
2019-06-04 14:40:01 -0400

    use create index for mongoose

--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get("mongoURI");
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
-    console.log("MongoDB connected ...")
-  } catch(err) {
+    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+    console.log('MongoDB connected ...');
+  } catch (err) {
     console.log(err.message);
 
     // Exit process with failure
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
