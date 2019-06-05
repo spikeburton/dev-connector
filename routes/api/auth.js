7ccdd8a2d332de4ecda396d919dcb38ea13b95f9
spikeburton <ralphburton3@gmail.com>
2019-06-04 22:45:26 -0400

    add route to login user with post request to auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -50,7 +50,7 @@ router.post(
 
       if (!user) {
         return res
-          .status(401)
+          .status(400)
           .json({ errors: [{ msg: 'Invalid Credentials.' }] });
       }
 
@@ -58,7 +58,7 @@ router.post(
 
       if (!isMatch) {
         return res
-          .status(401)
+          .status(400)
           .json({ errors: [{ msg: 'Invalid Credentials.' }] });
       }
 
