1e91df7946b60d0e4a96b9b07e3f8b3f08c57861
spikeburton <ralphburton3@gmail.com>
2019-06-05 22:26:59 -0400

    do not use find and modify

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false
+     });
     console.log('MongoDB connected ...');
   } catch (err) {
     console.log(err.message);
