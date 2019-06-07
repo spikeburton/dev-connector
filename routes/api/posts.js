bac5d896c4fcef521fb4eed9715f998e0d9d183e
spikeburton <ralphburton3@gmail.com>
2019-06-07 15:17:19 -0400

    add route to delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -61,7 +61,7 @@ router.get('/', auth, async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
-})
+});
 
 /**
  * @route   GET api/posts/:id
@@ -86,6 +86,38 @@ router.get('/:id', auth, async (req, res) => {
 
     res.status(500).send('Server Error');
   }
-})
+});
+
+/**
+ * @route   DELETE api/posts/:id
+ * @desc    Delete post by ID
+ * @access  Private
+ */
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).send({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
