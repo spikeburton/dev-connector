e4a1735eb59a813775b089a7d747e95049d5d24b
spikeburton <ralphburton3@gmail.com>
2019-06-08 15:57:49 -0400

    add and remove comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,6 @@ const { check, validationResult } = require('express-validator/check');
 const auth = require('../../middleware/auth');
 
 const Post = require('../../models/Post');
-const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 /**
@@ -190,5 +189,88 @@ router.put('/unlike/:id', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+/**
+ * @route   POST api/posts/comment/:id
+ * @desc    Comment on a post
+ * @access  Private
+ */
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+/**
+ * @route   DELETE api/posts/comment/:id/:comment_id
+ * @desc    Delete a comment on a post
+ * @access  Private
+ */
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    const i = post.comments.indexOf(comment);
+    if (i === -1) {
+      return res.status(400).json({ msg: 'No such comment ID' });
+    }
+
+    post.comments.splice(i, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send('No such ID');
+    }
+
+    res.status(500).send('Server Error');
+  }
+})
 
 module.exports = router;
