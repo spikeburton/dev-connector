b4dbd45d9102fc5d18f19f0f0f3e11465821ee13
spikeburton <ralphburton3@gmail.com>
2019-06-08 13:55:32 -0400

    like/unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -120,4 +120,75 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+/**
+ * @route   PUT api/posts/like/:id
+ * @desc    Like a post
+ * @access  Private
+ */
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).send('No post found with that ID');
+    }
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).send({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+    // res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send('No post found with that ID');
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route   PUT api/posts/unlike/:id
+ * @desc    Unlike a post
+ * @access  Private
+ */
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).send('No post found with that ID');
+    }
+
+    const i = post.likes.map(like => like.user.toString()).indexOf(req.user.id)
+    if (i === -1) {
+      return res.status(400).send({ msg: 'Post has not yet been liked' });
+    }
+
+    post.likes.splice(i, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+    // res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send('No post found with that ID');
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
