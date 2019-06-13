544c0da5c6c15545b29f6cb353147cbf4fc7a440
spikeburton <ralphburton3@gmail.com>
2019-06-13 17:28:39 -0400

    implement alert action, render alert component in App

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert'
+
 import './App.css';
 
 const App = () => (
@@ -12,6 +14,7 @@ const App = () => (
       <Navbar />
       <Route exact path="/" component={Landing} />
       <section className="container">
+        <Alert />
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
