1e346ff2841ec823866da4e6f41640802f34efea
spikeburton <ralphburton3@gmail.com>
2019-06-14 11:06:28 -0400

    check for token and set common auth header

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,16 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken'
 
 import { Provider } from 'react-redux';
 import store from './store';
 
 import './App.css';
+
+if (localStorage.getItem('token')) {
+  setAuthToken(localStorage.getItem('token'));
+}
 
 const App = () => {
   useEffect(() => {
