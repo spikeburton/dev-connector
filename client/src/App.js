57887de459e66ee283bf216b420ed30405bc950d
spikeburton <ralphburton3@gmail.com>
2019-06-14 16:38:36 -0400

    add Dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken'
+import Dashboard from './components/dashboard/Dashboard'
 
 import { Provider } from 'react-redux';
 import store from './store';
+
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken'
 
 import './App.css';
 
@@ -31,6 +33,7 @@ const App = () => {
           <section className="container">
             <Alert />
             <Switch>
+              <Route exact path="/dashboard" component={Dashboard} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
             </Switch>
