c1b2241b56fc617636e69cc53b1148d9f7b8cb3c
spikeburton <ralphburton3@gmail.com>
2019-06-14 17:44:43 -0400

    add profile action types

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,5 @@
-import { type } from "os";
+import { type } from 'os';
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -6,12 +7,24 @@ const initialState = {
   repos: [],
   loading: true,
   error: {}
-}
+};
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false
+      };
     default:
       return state;
   }
