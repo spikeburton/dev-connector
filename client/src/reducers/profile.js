56175f9d5cd5eb4a189e4f7d24a25166ee05064c
spikeburton <ralphburton3@gmail.com>
2019-06-14 18:49:18 -0400

    add clear profile action type

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,5 @@
 import { type } from 'os';
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -25,6 +25,13 @@ export default function(state = initialState, action) {
         error: payload,
         loading: false
       };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      }
     default:
       return state;
   }
