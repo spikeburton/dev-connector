09649d80e000fa534b2827a53b511c357175897b
spikeburton <ralphburton3@gmail.com>
2019-06-15 13:17:04 -0400

    add private route for edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,16 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import Dashboard from './components/dashboard/Dashboard'
-import CreateProfile from './components/profile-forms/CreateProfile'
-import PrivateRoute from './components/routing/PrivateRoute'
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import { Provider } from 'react-redux';
 import store from './store';
 
 import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken'
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
@@ -38,7 +39,16 @@ const App = () => {
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
             </Switch>
           </section>
         </Fragment>
