3471ef2c53a788ee71180cc80dd4ca7df2d96bab
spikeburton <ralphburton3@gmail.com>
2019-06-14 20:04:18 -0400

    add private route for CreateProfile

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/profile-forms/CreateProfile'
 import PrivateRoute from './components/routing/PrivateRoute'
 
 import { Provider } from 'react-redux';
@@ -37,6 +38,7 @@ const App = () => {
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
