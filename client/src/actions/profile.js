217a19ddef8e0a2f55df580d469a88c4c1897301
spikeburton <ralphburton3@gmail.com>
2019-06-24 23:53:50 -0400

    add experience to profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 
 //  Get current user profile
 export const getCurrentProfile = () => {
@@ -32,11 +32,48 @@ export const createProfile = (formData, history, edit = false) => {
       const res = await axios.post('/api/profile', formData, config);
 
       dispatch({ type: GET_PROFILE, payload: res.data });
-      dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+      dispatch(
+        setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+      );
 
       if (!edit) {
         history.push('/dashboard');
       }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+};
+
+// Add Experience
+export const addExperience = (formData, history) => {
+  return async dispatch => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      const res = await axios.put('/api/profile/experience', formData, config);
+
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data
+      });
+
+      dispatch(setAlert('Experience Added', 'success'));
+
+      history.push('/dashboard');
     } catch (err) {
       const errors = err.response.data.errors;
 
