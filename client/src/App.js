9fed1bab360ccf3327f864775e0324670e4881b8
spikeburton <ralphburton3@gmail.com>
2019-07-01 20:55:09 -0400

    add route for /profiles

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import AddExperience from './components/AddExperience';
 import AddEducation from './components/AddEducation';
+import Profiles from './components/profiles/Profiles';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -40,6 +41,7 @@ const App = () => {
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/profiles" component={Profiles} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
