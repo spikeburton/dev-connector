112232e55497da886b0523ad2fe1947357dddb07
spikeburton <ralphburton3@gmail.com>
2019-07-01 21:30:29 -0400

    adjust spacing of text

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -16,7 +16,7 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
       {loading ? <Spinner /> : <Fragment>
         <h1 className="large text-primary">Developers</h1>
         <p className="lead">
-          <i className="fab fa-connectdevelop" />Browse and connect with developers
+          <i className="fab fa-connectdevelop" /> Browse and connect with developers
         </p>
         <div className="profiles">
           { profiles.length > 0 ? (
