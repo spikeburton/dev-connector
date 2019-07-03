521885ae665a9db24f50dc3000de0355204db929
spikeburton <ralphburton3@gmail.com>
2019-07-03 17:52:41 -0400

    add button to edit profile if authenticated and authorized

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 
@@ -14,10 +16,24 @@ const Profile = ({
     getProfileById(match.params.id);
   }, [getProfileById]);
 
-  console.log(match.params);
   return (
     <Fragment>
-      {!profile || loading ? <Spinner /> : <Fragment>profile</Fragment>}
+      {!profile || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            !auth.loading &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
     </Fragment>
   );
 };
