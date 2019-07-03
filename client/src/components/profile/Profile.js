69abb0a0076df53ad1cd198165729d36f293e38a
spikeburton <ralphburton3@gmail.com>
2019-07-03 18:26:49 -0400

    render top of profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -14,7 +15,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
@@ -32,6 +33,9 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+            <div className="profile-grid my-1">
+              <ProfileTop profile={profile} />
+            </div>
         </Fragment>
       )}
     </Fragment>
