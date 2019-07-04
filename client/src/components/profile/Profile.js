b1b8fc79f91b551110e95c893e808efbd0c91a32
spikeburton <ralphburton3@gmail.com>
2019-07-03 20:29:05 -0400

    render experiences from array as presentational component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -34,10 +35,22 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
-            <div className="profile-grid my-1">
-              <ProfileTop profile={profile} />
-              <ProfileAbout profile={profile} />
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(exp => (
+                    <ProfileExperience key={exp._id} experience={exp} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
             </div>
+          </div>
         </Fragment>
       )}
     </Fragment>
