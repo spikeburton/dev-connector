6f7d4559c8e0df872204efa3e91b406724df4797
spikeburton <ralphburton3@gmail.com>
2019-07-12 15:09:03 -0400

    implement click handlers for like buttons

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
 const PostItem = ({
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date }
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  addLike,
+  removeLike
 }) => {
   return (
     <div className="post bg-white p-1 my-1">
@@ -21,11 +24,19 @@ const PostItem = ({
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button type="button" className="btn btn-light">
+        <button
+          type="button"
+          className="btn btn-light"
+          onClick={() => addLike(_id)}
+        >
           <i className="fas fa-thumbs-up" />{' '}
           {likes.length > 0 && <span>{likes.length}</span>}
         </button>
-        <button type="button" className="btn btn-light">
+        <button
+          type="button"
+          className="btn btn-light"
+          onClick={() => removeLike(_id)}
+        >
           <i className="fas fa-thumbs-down" />
         </button>
         <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -55,5 +66,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {}
+  { addLike, removeLike }
 )(PostItem);
