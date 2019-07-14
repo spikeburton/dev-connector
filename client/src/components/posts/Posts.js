d7e1a66b24bc3433ab7299a13469b4921ed059cb
spikeburton <ralphburton3@gmail.com>
2019-07-14 19:18:56 -0400

    render post form

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
+import PostForm from './PostForm';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -16,7 +17,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <p className="lead">
       <i className="fas fa-user" /> Welcome to the community
     </p>
-    {/* PostForm */}
+    <PostForm />
     {posts.map(post => (
       <PostItem key = {post._id} post={post} />
     ))}
