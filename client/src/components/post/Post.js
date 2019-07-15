5b672023a766f6f72d1282e471118863c0984018
spikeburton <ralphburton3@gmail.com>
2019-07-14 22:01:18 -0400

    render comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -24,9 +24,9 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
       <div className="comments">
-        {post.comments.map(comment => {
-          <CommentItem key={comment._id} comment={comment} postId={post._id} />;
-        })}
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
       </div>
     </Fragment>
   );
